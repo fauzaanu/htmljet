bc17e29932f41607e8dc8ee201cd35e5a8f01ba8
fauzaanu <hello@fauzaanu.com>
2024-10-03 07:07:23 +0500

    feat: add main.py to the project

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+elementor_snaps is a python script designed to take screenshots of Elementor pages.
+
+The default class we will be taking screenshots from is the elementor-element class as that is usually the class that
+contains one singular component.
+
+Why?
+---
+This script was created to automate the process of taking screenshots of Elementor pages. This is useful for recreating
+the UI of elementor websites using LLMs for other purposes.
+
+The project only has one dependency, which is the playwright library.
+"""
+
+
+
